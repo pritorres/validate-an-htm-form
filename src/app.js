fa30b62d1c441b1c60bb8dadf1d9329445e9841f
priscila torres reyes <priscyla.torres16@hotmail.com>
2021-09-05 19:00:13 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -1,24 +1,14 @@
 /* eslint-disable */
 import "bootstrap";
+
 import "./style.css";
 
-const expresiones = {
-  usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-  nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-  password: /^.{4,12}$/, // 4 a 12 digitos.
-  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-  telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-};
-
 const myForm = document.getElementById("myFormulario");
-myForm.addEventListener("submit", evento => {
-  evento.preventDefault();
-  console.log("sasa");
-  let cardNro = document.querySelector("#CardNro");
-  let cardNroError = document.querySelector("#CardNro");
 
-  console.log(cardNro.value);
-  if (cardNro.value === "") {
-    cardNroError.innerHTML = "CardNro is required";
+myForm.addEventListener("submit", event => {
+  event.preventDefault();
+  event.stopPropagation();
+  if (myForm.checkValidity() === false) {
   }
+  myForm.classList.add("was-validated");
 });
